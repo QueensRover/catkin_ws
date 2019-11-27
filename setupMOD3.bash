6f07356acfcf933d15372e12cba08472572a1db6
Owen Hooper <17ofh@queensu.ca>
2019-11-27 16:28:36 -0500

    back to kinetic

--- setupMOD3.bash
+++ setupMOD3.bash
@@ -1,17 +1,18 @@
+sudo apt install x11-apps
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
-sudo apt-get install ros-melodic-desktop-full
+sudo apt-get install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install python-catkin-tools
-source /opt/ros/melodic/setup.bash
+source /opt/ros/kinetic/setup.bash
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/
 catkin init
@@ -31,12 +32,12 @@ git clone https://gitlab.com/qset_rover/ros_sabertooth
 cd ~/catkin_ws
 catkin build owen_launch hector_gazebo_plugins wheel_control tf_transforms 
 cp ~/catkin_ws/src/ros-gazebo/plugins/build/*.so ~/catkin_ws/devel/lib
-sudo cp ~/catkin_ws/src/ros-gazebo/rover_real_model /usr/share/gazebo-9/models/
+sudo cp -r ~/catkin_ws/src/ros-gazebo/rover_real_model /usr/share/gazebo-9/models/
 sudo echo "export GAZEBO_PLUGIN_PATH=~/catkin_ws/devel/lib" >> ~/.bashrc
 source ~/catkin_ws/devel/setup.bash
 sudo apt install libjansson-dev nodejs npm nodejs-legacy libboost-dev imagemagick libtinyxml-dev mercurial cmake build-essential
 cd ~; hg clone https://bitbucket.org/osrf/gzweb
 cd ~/gzweb
-hg up gzweb_1.4.0
+sudo hg up gzweb_1.4.0
 source /usr/share/gazebo/setup.sh
 npm run deploy --- -m
