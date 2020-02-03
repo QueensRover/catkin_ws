e80d0e255021f0820e406113cea89385701090fe
Owen Hooper <17ofh@queensu.ca>
2020-02-03 15:15:38 -0500

    made more script changes

--- fixHeading.bash
+++ fixHeading.bash
@@ -1,3 +1,4 @@
+rm ~/catkin_ws/devel/libhector*
 cp ~/unified-launch/gazebo-launch/worlds/world_with_rover.world ~/catkin_ws/src/gazebo-launch/worlds/
 cp ~/unified-launch/gazebo_plugins/hector_gazebo_plugins/src/gazebo_ros_magnetic.cpp ~/catkin_ws/src/gazebo_plugins/hector_gazebo_plugins/src/
 cd ~/catkin_ws
