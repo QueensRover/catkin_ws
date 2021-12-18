1cdba0ecdb49b9f98c52abe9004aabdc010b195f
Owen Hooper <17ofh@queensu.ca>
2021-12-18 15:44:27 -0500

    installer changes

--- installer.sh
+++ installer.sh
@@ -3,4 +3,7 @@
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 sudo apt install git
+rm -rf unified-launch
 git clone https://gitlab.com/qset-mod-3/unified-launch
+cd ~/catkin_ws/src/unified_launch
+./setupMOD3.bash
