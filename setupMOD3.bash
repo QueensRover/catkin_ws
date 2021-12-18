2099fe4382b24f8c5381eccef3cfc834a25278f4
Owen Hooper <17ofh@queensu.ca>
2021-12-18 16:03:50 -0500

    changed sim

--- setupMOD3.bash
+++ setupMOD3.bash
@@ -14,25 +14,21 @@ sudo apt-get update
 sudo apt-get -y install python-catkin-tools
 
 source /opt/ros/melodic/setup.bash
-source ~/.bashrc
-cd ~/catkin_ws/
-catkin_make
-source devel/setup.bash
-sudo echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+cd ~/catkin_ws
+rosdep install --from-paths src --ignore-src -r -y
 cd ~/catkin_ws/src/unified-launch
-sudo rm -r ./.git
-source /usr/share/gazebo/setup.sh
+source ~/.bashrc
 cd ~/catkin_ws/
 catkin init
 catkin clean -y
-catkin build owen_launch hector_gazebo_plugins wheel_control tf_transforms 
-cd ~/catkin_ws/src/ros-gazebo/plugins/build
+catkin build 
+cd ~/catkin_ws/src/unified-launch/ros-gazebo/plugins/build
 sudo rm ./CMake*
 sudo cmake ..
 sudo make
-cp ~/catkin_ws/src/ros-gazebo/plugins/build/*.so ~/catkin_ws/devel/lib
+cp ~/catkin_ws/src/unified-launch/ros-gazebo/plugins/build/*.so ~/catkin_ws/devel/lib
 sudo cp -r ~/catkin_ws/src/ros-gazebo/rover_real_model /usr/share/gazebo-9/models/
 echo "export GAZEBO_PLUGIN_PATH=~/catkin_ws/devel/lib" >> ~/.bashrc
-echo "export DISPLAY=:0" >> ~/.bashrc
 
 
