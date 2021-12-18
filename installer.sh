73931f879c4be15fffb0c73c0c7a370b11dbc894
Owen Hooper <17ofh@queensu.ca>
2021-12-18 16:34:20 -0500

    changed sim

--- installer.sh
+++ installer.sh
@@ -5,5 +5,6 @@ cd ~/catkin_ws/src
 sudo apt install git
 rm -rf unified-launch
 git clone https://gitlab.com/qset-mod-3/unified-launch
+git clone https://gitlab.com/qset-mod-3/qset-lib
 cd ~/catkin_ws/src/unified-launch
 ./setupMOD3.bash
