fd6bfe350c970d0f6b1bf8a2206c6d6254306215
Owen Hooper <17ofh@queensu.ca>
2022-12-22 18:11:58 +0000

    Update file installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-sudo apt install git
+sudo apt install -y git
 rm -rf unified-launch
 git clone https://gitlab.com/qset-mod-3/unified-launch
 git clone https://gitlab.com/qset-mod-3/qset-lib
