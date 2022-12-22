f3a394ccfa298072038b4ba7d7095e8245c04868
Owen Hooper <17ofh@queensu.ca>
2022-12-22 13:14:36 -0500

    added lsb install

--- setupMOD3.bash
+++ setupMOD3.bash
@@ -1,5 +1,6 @@
 #! /bin/bash
-sudo apt install x11-apps
+sudo apt install -y x11-apps
+sudo apt install -y lsb
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get update
