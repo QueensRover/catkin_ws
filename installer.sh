39e4addc7d1c28efcccc01f6494065389f3068d6
Owen Hooper <17ofh@queensu.ca>
2022-12-22 14:05:38 -0500

    changed install order

--- installer.sh
+++ installer.sh
@@ -2,7 +2,8 @@
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-sudo apt install -y git
+sudo apt update
+sudo apt install -y git lsb
 rm -rf unified-launch
 git clone https://gitlab.com/qset-mod-3/unified-launch
 git clone https://gitlab.com/qset-mod-3/qset-lib
