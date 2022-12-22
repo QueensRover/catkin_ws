4e1f0a6d4fc6e9d894b6855e101cd4d20a789a4a
Owen Hooper <17ofh@queensu.ca>
2022-12-22 15:59:32 -0500

    Tested install and example alg

--- setupMOD3.bash
+++ setupMOD3.bash
@@ -5,7 +5,7 @@ sudo apt install -y gnupg
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y ros-melodic-desktop-full
+sudo apt-get install -y ros-melodic-desktop-full cmake
 sudo apt-get install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
@@ -32,5 +32,6 @@ catkin init
 catkin clean -y
 catkin build 
 echo "export GAZEBO_PLUGIN_PATH=~/catkin_ws/devel/lib" >> ~/.bashrc
+source ~/.bashrc
 
 
