21b47687aca6a51982fceb27a985009d1992c890
Owen Hooper <17ofh@queensu.ca>
2023-01-03 16:35:26 -0500

    added comments to the example

--- code_snipets/example_alg.py
+++ code_snipets/example_alg.py
@@ -2,7 +2,7 @@ from qset_lib import Rover
 from time import sleep
 
 def main():
-    rover = Rover()
+    rover = Rover() # this line starts the connection to the rover and gives access to the rover data
 
     i = 0
 
@@ -10,11 +10,16 @@ def main():
     right_side_speed = 1
 
     while i < 1000:
-        print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
+        print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading)) # this line prints the current location and heading of the rover
+
+        # the below lines iterate through all the laser scan lines and prints if the distance is less than 0.1 meters
         for dist in rover.laser_distances:
             if dist < 0.1:
                 print("TOO CLOSE")
+
+        # the below line sends a command to the rover (simulation) 
         rover.send_command(left_side_speed, right_side_speed)
+
         i = i + 1
         sleep(0.01)
 
