f32fc0afcd4f475cf558c9ebbb26a2ed14b610a3
Owen Hooper <17ofh@queensu.ca>
2023-03-07 11:27:18 -0500

    Added angle reader

--- code_snipets/example_alg.py
+++ code_snipets/example_alg.py
@@ -1,9 +1,10 @@
-from qset_lib import Rover
+from qset_lib import Rover, AngleReader
 from time import sleep
 import signal
 
 def main():
     rover = Rover() # this line starts the connection to the rover and gives access to the rover data
+    angle_reader = AngleReader()
 
     # This line allows for the KeyboardInterrupt to worl
     signal.signal(signal.SIGINT, signal.default_int_handler)
@@ -13,7 +14,8 @@ def main():
     right_side_speed = 1
     isTooClose = False
     try:
-        while not isTooClose:
+        while not isTooClose or True:
+            print(" ANGLE: " + str(angle_reader.read_angle))
             # this line prints the current location and heading of the rover
             print("X: " + str(rover.x) + " Y: " + str(rover.y) + " Heading: " + str(rover.heading))
             # the below lines iterate through all the laser scan lines and prints if the distance is less than 0.5 meters
